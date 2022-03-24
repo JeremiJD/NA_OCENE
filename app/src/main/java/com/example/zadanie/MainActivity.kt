f9b7df54628d41e582cd5d1ab5f799a54451f9b9
JeremiJD <jeremidjeremi@gmail.com>
2022-03-24 12:49:46 +0100

    działanie

--- app/src/main/java/com/example/zadanie/MainActivity.kt
+++ app/src/main/java/com/example/zadanie/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.zadanie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.RadioButton
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,12 +11,68 @@ class MainActivity : AppCompatActivity() {
 
         val show = findViewById<Button>(R.id.show_btn);
         val unblck = findViewById<Button>(R.id.odblokuj)
-        val wiek =  findViewById<EditText>(R.id.Imie)
+        val wiek =  findViewById<EditText>(R.id.Imie).toString().toInt()
+
         val man = findViewById<RadioButton>(R.id.men)
         val woman = findViewById<RadioButton>(R.id.kobieta)
         val sport = findViewById<CheckBox>(R.id.Sport)
         val gry = findViewById<CheckBox>(R.id.Gry)
         val muzyka = findViewById<CheckBox>(R.id.Muzyka)
         val tur = findViewById<CheckBox>(R.id.Turystyka)
+        var wyniki = findViewById<TextView>(R.id.Wyniki)
+
+
+        show.setOnClickListener{
+            //Płeć
+            if (man.isChecked){
+                wyniki.append("męzczycna\n")
+
+            }
+            if (woman.isChecked){
+                wyniki.append("Kobieta\n")
+            }
+            //wiek
+            wyniki.append(wiek.toString()+"Lat\n")
+            wyniki.append("Hobby\n")
+            //Zainteresowania
+            if (gry.isChecked){
+                wyniki.append("-Gry\n")
+            }
+            if (muzyka.isChecked){
+                wyniki.append("-Muzyka\n")
+            }
+            if (tur.isChecked){
+                wyniki.append("-Turystyka\n")
+            }
+            if (sport.isChecked){
+                wyniki.append("-Sport\n")
+            }
+
+
+
+            //Blokowanie
+
+            woman.isEnabled = false
+            man.isEnabled = false
+            //wiek.isEnabled = false
+            sport.isEnabled = false
+            gry.isEnabled = false
+            muzyka.isEnabled = false
+            tur .isEnabled = false
+
+
+
+        }
+
+        unblck.setOnClickListener {
+            woman.isEnabled = true
+            man.isEnabled = true
+            wiek.isEnabled = true
+            sport.isEnabled = true
+            gry.isEnabled = true
+            muzyka.isEnabled = true
+            tur.isEnabled = true
+
+        }
     }
 }
